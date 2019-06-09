f6b53d99a773e5da79c2896805dd91aea0879b0f
Rafał Czarnecki <s16281@pjwstk.edu.pl>
2019-06-09 12:13:24 +0200

    app.module.ts path fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { PersonTableComponent } from './person-table/person-table.component';
 import { PersonTableRowComponent } from './person-table-row/person-table-row.component';
 import { PersonTableRowSortComponent } from './person-table-row-sort/person-table-row-sort.component';
-import { PersonService } from "app/services/person-service";
+import { PersonService } from "./services/person-service";
 import { PersonTablePagingComponent } from './person-table-paging/person-table-paging.component';
 
 @NgModule({
